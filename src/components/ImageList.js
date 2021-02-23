2342d147bd62864b1b02f77c4a086ee15dd2b9cf
Johnney Chen <mzchenca@gmail.com>
2021-02-22 18:51:49 -0700

    added formatting to make images fit

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = props => {
-    let imgs = props.data.map(({ description, urls, id }) => <img alt={description} src={urls.regular} key={id} />)
+    let imgs = props.data.map((data) => <ImageCard image={data} key={data.id} />)
     return (
-        <div>
+        <div className="image-list">
             {imgs}
         </div>
     )
